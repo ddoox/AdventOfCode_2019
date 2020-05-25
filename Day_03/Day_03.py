af98a86a58bad5c7ff7c7d0f03b631b2686b0440
ddoox <dudek746@gmail.com>
2020-05-25 10:31:18 +0200

    Day 3 part II

--- Day_03/Day_03.py
+++ Day_03/Day_03.py
@@ -40,18 +40,54 @@ wire2.remove([0, 0])
 uniq_wire1 = set(tuple(row) for row in wire1)
 uniq_wire2 = set(tuple(row) for row in wire2)
 
+# print(wire1)
 first = True
+intersections = []
 
 for coordinates in uniq_wire1:
     if coordinates in uniq_wire2:
+        intersections.append(coordinates)
         temp = abs(coordinates[0]) + abs(coordinates[1])
         if first:
             closest = temp
             first = False
         elif temp < closest:
             closest = temp
-            print("Temp: " + str(closest))
+            # print("Temp: " + str(closest))
 
 print("True: " + str(closest))
+
+#   -   -   -   Part 2  -   -   -   -#
+
+first = True
+min_distance = 0
+wire1_distance = 0
+wire2_distance = 0
+
+for intersection in intersections:
+
+    for i in range(0, len(wire1)):
+        if wire1[i][0] == intersection[0] and wire1[i][1] == intersection[1]:
+            wire1_distance = i + 1
+    for k in range(0, len(wire2)):
+        if wire2[k][0] == intersection[0] and wire2[k][1] == intersection[1]:
+
+            wire2_distance = k + 1
+
+    temp_dist = wire1_distance + wire2_distance
+    if first:
+        min_distance = temp_dist
+        first = False
+    elif temp_dist < min_distance:
+        min_distance = temp_dist
+
+print(min_distance)
+
+
+
+# if wire1[1][0] == intersections[4][0]:
+#     print("k")
+
+
 
 
