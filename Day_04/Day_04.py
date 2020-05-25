49d309723bc3cf762583df28181a97164a456308
ddoox <dudek746@gmail.com>
2020-05-25 11:44:13 +0200

    Day 4 complete

---
+++ Day_04/Day_04.py
@@ -0,0 +1,34 @@
+input_min = 153517
+input_max = 630395
+
+meet_criteria = 0
+
+for number in range(input_min, input_max + 1):
+    criteria = True
+    has_double = False
+    first_char = True
+    second_char = False
+    char_previous = "9"
+    char_penultimate = "9"
+    double_number = "9"
+
+    number = str(number)
+
+    for char in number:
+        if first_char:
+            char_previous = char
+            first_char = False
+        else:
+            if char == char_previous:
+                if number.count(char) == 2:
+                    has_double = True
+        if char < char_previous:
+            criteria = False
+            break
+
+        char_previous = char
+
+    if criteria and has_double:
+        meet_criteria += 1
+
+print(meet_criteria)
