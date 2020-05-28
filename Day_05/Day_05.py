2f0c405301e2bccf33b15a9a069020d82c83b963
ddoox <dudek746@gmail.com>
2020-05-28 18:33:03 +0200

    Still haven't found what I'm looking for, but code looks better

--- Day_05/Day_05.py
+++ Day_05/Day_05.py
@@ -1,42 +1,36 @@
-input_file = [3, 0, 4, 0, 99]
+# input_file = [3,0,4,0,99]
+# input_file = [1002,4,3,4,33]
 swap = []
 with open("input", "r") as input_file:
     input_file = input_file.read()
     input_file = input_file.strip().split(",")
     for number in input_file:
         swap.append(int(number))
-input_file = swap.copy()
+input_file = [_ for _ in swap]
 
 # 36312386 - bad
+# 16225258 - yey
 
 input_value = 1
 result_index = 0
 opcode_index = 0
-# opcode_index = 182
-# opcode_index = 10
-# opcode_index = 12
-# opcode_index = 58
-# opcode_index = 80
-# opcode_index = 102
-# opcode_index = 126
-# opcode_index = 150
-# opcode_index = 206
-# opcode_index = 220
-previous_opcode = 0
 
-opcode_value_str = input_file[opcode_index].__str__()
-opcode_value_int = int(opcode_value_str[opcode_value_str.__len__() - 1])
+
+opcode_value_str = str(input_file[opcode_index])
+opcode_value_int = int(opcode_value_str[-1])
 output = 0
 param_B = 0
 param_C = 0
 
-while opcode_value_int == 1 or opcode_value_int == 2 or opcode_value_int == 3 or opcode_value_int == 4:
+while opcode_value_int in [1, 2, 3, 4]:
 
-    if opcode_value_str.__len__() == 3:
-        param_C = int(opcode_value_str[opcode_value_str.__len__() - 3])
-    elif opcode_value_str.__len__() == 4:
-        param_C = int(opcode_value_str[opcode_value_str.__len__() - 3])
-        param_B = int(opcode_value_str[opcode_value_str.__len__() - 4])
+    if opcode_index == 220:
+        print("ends here")
+    if len(opcode_value_str) == 3:
+        param_C = int(opcode_value_str[-3])
+    elif len(opcode_value_str) == 4:
+        param_C = int(opcode_value_str[-3])
+        param_B = int(opcode_value_str[-4])
     else:
         param_B = 0
         param_C = 0
@@ -46,40 +40,33 @@ while opcode_value_int == 1 or opcode_value_int == 2 or opcode_value_int == 3 or
     elif opcode_value_int in [3, 4]:
         param_C = input_file[input_file[opcode_index + 1]]
     elif param_B and param_C:
-        param_B = input_file[opcode_index + 1]
-        param_C = input_file[opcode_index + 2]
-    elif param_C:
-        param_B = input_file[input_file[opcode_index + 2]]
         param_C = input_file[opcode_index + 1]
-    elif param_B:
         param_B = input_file[opcode_index + 2]
+    elif param_C:
+        param_C = input_file[opcode_index + 1]
+        param_B = input_file[input_file[opcode_index + 2]]
+    elif param_B:
         param_C = input_file[input_file[opcode_index + 1]]
+        param_B = input_file[opcode_index + 2]
     else:
         param_C = input_file[input_file[opcode_index + 1]]
         param_B = input_file[input_file[opcode_index + 2]]
 
-    if opcode_value_int <= 2:
-        result_index = input_file[opcode_index + 3]
-        if opcode_value_int == 1:
-            input_file[result_index] = param_B + param_C
-        else:
-            input_file[result_index] = param_B * param_C
-        opcode_index += 4
-    elif opcode_value_int <= 4:
-        result_index = input_file[opcode_index + 1]
-        if opcode_value_int == 3:
-            input_file[result_index] = input_value
-        else:
-            output = input_file[result_index]
-            print("output: " + str(output))
-            print("opcode index: " + str(opcode_index))
-            print("pre index: " + str(previous_opcode) + "\n")
-        opcode_index += 2
+    result_index = input_file[opcode_index + 3] if opcode_value_int == 1 or opcode_value_int == 2 else input_file[opcode_index + 1]
+    if opcode_value_int == 1:
+        input_file[result_index] = param_B + param_C
+    elif opcode_value_int == 2:
+        input_file[result_index] = param_B * param_C
+    elif opcode_value_int == 3:
+        input_file[result_index] = input_value
+    elif opcode_value_int == 4:
+        output = input_file[result_index]
+        print("output: " + str(output))
+        print("opcode index: " + str(opcode_index) + "\n")
 
-    previous_opcode = opcode_index
-    opcode_value_str = input_file[opcode_index].__str__()
-    opcode_value_int = int(opcode_value_str[opcode_value_str.__len__() - 1])
-    # print(input_file)
+    opcode_index += 4 if opcode_value_int == 1 or opcode_value_int == 2 else 2
 
+    opcode_value_str = str(input_file[opcode_index])
+    opcode_value_int = int(opcode_value_str[-1])
 
 print(output)
