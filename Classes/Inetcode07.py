5455b3c40b0f785f30bf54102fde7de3d3f095a9
ddoox <dudek746@gmail.com>
2020-06-03 10:31:34 +0200

    Day 7 part I complete

--- Classes/Inetcode07.py
+++ Classes/Inetcode07.py
@@ -14,25 +14,30 @@ class Inetcode():
 
     def calc(self):
 
-        # input_value = 1
         opcode_index = 0
-
         opcode_value_str = str(self.input_file[opcode_index])
         opcode_value_int = int(opcode_value_str[-1])
         output = 0
         give_phase = True
 
-        while opcode_value_int in [1, 2, 3, 4]:
+        while opcode_value_int in [1, 2, 3, 4, 5, 6, 7, 8]:
 
             if len(opcode_value_str) == 3:
                 param_C = int(opcode_value_str[-3])
                 param_B = 0
+                # param_A = 0
             elif len(opcode_value_str) == 4:
                 param_C = int(opcode_value_str[-3])
                 param_B = int(opcode_value_str[-4])
+                # param_A = 0
+            elif len(opcode_value_str) == 5:
+                param_C = int(opcode_value_str[-3])
+                param_B = int(opcode_value_str[-4])
+                # param_A = int(opcode_value_str[-5])
             else:
-                param_B = 0
                 param_C = 0
+                param_B = 0
+                # param_A = 0
 
             if opcode_value_int in [3, 4] and param_C:
                 param_C = self.input_file[opcode_index + 1]
@@ -51,8 +56,11 @@ class Inetcode():
                 param_C = self.input_file[self.input_file[opcode_index + 1]]
                 param_B = self.input_file[self.input_file[opcode_index + 2]]
 
-            result_index = self.input_file[opcode_index + 3] if opcode_value_int == 1 or opcode_value_int == 2 else self.input_file[
-                opcode_index + 1]
+            if opcode_value_int in [1, 2, 7, 8]:
+                result_index = self.input_file[opcode_index + 3]
+            elif opcode_value_int in [3, 4]:
+                result_index = self.input_file[opcode_index + 1]
+
             if opcode_value_int == 1:
                 self.input_file[result_index] = param_B + param_C
             elif opcode_value_int == 2:
@@ -67,7 +75,25 @@ class Inetcode():
                 output = self.input_file[result_index]
                 # print("output: " + str(output))
                 # print("opcode index: " + str(opcode_index) + "\n")
-            opcode_index += 4 if opcode_value_int == 1 or opcode_value_int == 2 else 2
+            elif opcode_value_int == 5:
+                if param_C != 0:
+                    opcode_index = param_B
+                else:
+                    opcode_index += 3
+            elif opcode_value_int == 6:
+                if param_C == 0:
+                    opcode_index = param_B
+                else:
+                    opcode_index += 3
+            elif opcode_value_int == 7:
+                self.input_file[result_index] = 1 if param_C < param_B else 0
+            elif opcode_value_int == 8:
+                self.input_file[result_index] = 1 if param_C == param_B else 0
+
+            if opcode_value_int in [1, 2, 7, 8]:
+                opcode_index += 4
+            elif opcode_value_int in [3, 4]:
+                opcode_index += 2
 
             opcode_value_str = str(self.input_file[opcode_index])
             opcode_value_int = int(opcode_value_str[-1])
--- Day_07/Day_07.py
+++ Day_07/Day_07.py
@@ -1,13 +1,20 @@
 from Classes.Inetcode07 import Inetcode
+from itertools import permutations
 
 
 def amplifier(phase, input_value):
     return Inetcode("../input", phase, input_value).calc()
 
 
-output = amplifier(0, 0)
-output = amplifier(1, output)
-output = amplifier(2, output)
-output = amplifier(3, output)
-output = amplifier(4, output)
-print(output)
+perms = permutations([0, 1, 2, 3, 4])
+max_output = 0
+
+for test in list(perms):
+    output = amplifier(test[0], 0)
+    output = amplifier(test[1], output)
+    output = amplifier(test[2], output)
+    output = amplifier(test[3], output)
+    output = amplifier(test[4], output)
+    if output > max_output:
+        max_output = output
+print(max_output)
